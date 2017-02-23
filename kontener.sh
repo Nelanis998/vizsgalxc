cf35b4cc367cedc169ce38b5b3f52ac75f8c3b62
Nelanis998 <e12.kiss.bianka.maria@palfy-vizugyi.hu>
2017-02-23 10:59:22 +0100

    Update kontener.sh

--- kontener.sh
+++ kontener.sh
@@ -2,6 +2,6 @@
 lxc init ubuntu:16.04 proba
 lxc start proba
 sleep 30
-lxc exec proba -- apt get install mc -y
-lxc exec proba -- apt get update -y
-lxc exec proba -- apt get upgrade -y
+lxc exec proba -- apt install mc -y
+lxc exec proba -- apt update -y
+lxc exec proba -- apt upgrade -y
